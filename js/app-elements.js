04dfd20a353ad5504c8f2f2baea6596a152883cd
wakaba6533 <wakabaku25@gmail.com>
2026-07-27 23:27:44 +0900

    refactor: 不要ファイルの整理

--- js/app-elements.js
+++ js/app-elements.js
@@ -10,21 +10,6 @@ const dom = {
     seriesPage: document.getElementById('seriesPage'),
     detailPage: document.getElementById('detailPage'),
     addPage: document.getElementById('addPage'),
-
-    // シリーズ画面
-    seriesTitle: document.getElementById('seriesTitle'),
-    seriesInfo: document.getElementById('seriesInfo'),
-    seriesBooks: document.getElementById('seriesBooks'),
-
-    // 詳細画面
-    detailTitle: document.getElementById('title'),
-    detailAuthor: document.getElementById('author'),
-    detailIsbn: document.getElementById('isbn'),
-    soldInfo: document.getElementById('soldInfo'),
-    editButton: document.getElementById('editButton'),
-    sellButton: document.getElementById('sellButton'),
-    deleteButton: document.getElementById('deleteButton'),
-    backButton: document.getElementById('backButton'),
 };
 
 window.dom = dom;
